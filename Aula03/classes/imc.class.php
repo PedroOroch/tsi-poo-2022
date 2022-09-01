a2dcf03c0a55ce3708fb2f17c1bb9f473663019e
Oroch <oliveirarocha_pedro@hotmail.com>
2022-09-01 01:37:51 -0300

    Exercicio finalizado, falta Front End

--- Aula03/classes/imc.class.php
+++ Aula03/classes/imc.class.php
@@ -21,13 +21,19 @@ class imc {
 
     public function getStats(){
         $imc = $this->getIMC();
-        if ($imc <= 18.5 || $imc <= 24.99 ) {
-            echo "Peso Normal";
-        } else if ($imc > 24 && $imc > 24.99 ) {
-            echo "Acima do Peso";
-        } else if ($imc > 30 && $imc > 34.99) {
-            echo "Você está Obeso";
+        if ($imc > 18.5 && $imc < 24.99 ) {
+            echo "Peso Normal\n";
+            echo "\n<img src='../EXERCICIOS/img/carinhafeliz1.jpg' alt='carinha feliz'>\n<img src='../EXERCICIOS/img/carinhafeliz2.jpg' alt='carinha feliz'>";
+        } else if ($imc > 25 && $imc < 29.99 ) {
+            echo "Acima do Peso\n";
+            echo "\n<img src='../EXERCICIOS/img/shrug.jpg' alt='¯\_(ツ)_/¯'>";
+        } else if ($imc > 30) {
+            echo "Você está Obeso\n";
+            echo "\n<img src='../EXERCICIOS/img/carinhatriste1.jpg' alt='carinha triste'>\n<img src='../EXERCICIOS/img/carinhatriste2.jpg' alt='carinha triste'>";
+        } else if ($imc < 18.5) {
+            echo "Abaixo do Peso\n";
+            echo "\n<img src='../EXERCICIOS/img/carinhatriste1.jpg' alt='carinha triste'>\n<img src='../EXERCICIOS/img/carinhafeliz1.jpg' alt='carinha feliz'>";
+
         }
     }
-
 }
